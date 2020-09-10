48c555e76d4d447eded1347d4b155eedcebb0e15
EC2 Default User <ec2-user@ip-172-31-3-236.ap-south-1.compute.internal>
2020-09-10 05:26:03 +0000

    new

---
+++ try.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+#######################################
+# Bash script to install apps on a new system (Ubuntu)
+# Written by @AamnahAkram from http://aamnah.com
+#######################################
+
+## Update packages and Upgrade system
+sudo apt-get update -y
+
+## Git ##
+echo '###Installing Git..'
+sudo apt-get install git -y
+
