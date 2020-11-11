d2c7a53eb699e700e67d493eb10f560ab6ebbf50
pratikinamake <36621212+pratikinamake@users.noreply.github.com>
2020-11-11 18:02:36 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,8 +1,8 @@
 echo "this is first test"
 
 echo "welcome "
-yum remove -y httpd
-yum remove nmap -y
+yum install -y httpd
+yum install nmap -y
 yum -y remove mariadb-server mariadb
 
 echo " complete"
