c1396e248e803a135c2e7566b21d73deae7fe372
pratikinamake <36621212+pratikinamake@users.noreply.github.com>
2020-11-11 18:08:15 +0530

    Update and rename shell.sh to Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM centos:latest
+
+CMD echo "welcome "
+RUN yum install -y httpd
+RUN yum install nmap -y
+CMD echo "complete"
--- shell.sh
+++
@@ -1,8 +0,0 @@
-echo "this is first test"
-
-echo "welcome "
-yum install -y httpd
-yum install nmap -y
-yum -y remove mariadb-server mariadb
-
-echo " complete"
